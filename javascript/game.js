0a385fa358f280f983fe1fa3b4d0b50e62aeba9e
William Lukas <iggyboy456@gmail.com>
2019-09-12 16:50:20 -0500

    updated formnatting with bootstrap grid to make things a little cleaner

--- javascript/game.js
+++ javascript/game.js
@@ -29,23 +29,23 @@ function getRandom(min, max) {
 //asigns random values to the target score and each crystal
 function assignValues() {
     randomNum = getRandom(19, 120);
-    crystal1 = getRandom(1,12);
-    crystal2 = getRandom(1,12);
-    crystal3 = getRandom(1,12);
-    crystal4 = getRandom(1,12);
+    crystal1 = getRandom(1, 12);
+    crystal2 = getRandom(1, 12);
+    crystal3 = getRandom(1, 12);
+    crystal4 = getRandom(1, 12);
 }
 
-//adds value of clicked crystal to score, and checks if player won, lost, or neither
+//adds value of clicked crystal to score, and checks if player won, lost, or neither. Also plays a certified banger 👌
 function getClick(num) {
     $("#banger")[0].play();
     score += num;
-    if(score === randomNum){
+    if (score === randomNum) {
         wins++;
         $("#message").text("You win!")
         score = 0;
         reset();
     }
-    else if (score > randomNum){
+    else if (score > randomNum) {
         losses++;
         $("#message").text("You lose!")
         score = 0;
@@ -54,26 +54,26 @@ function getClick(num) {
 }
 
 //prints all relevant values to their respective positions on the page
-function printer(){
-    $("#target-score").text(randomNum);
+function printer() {
+    $("#target-score").text("Target: " + randomNum);
     $("#wincounter").text("Wins: " + wins + " Losses: " + losses);
     $("#score-counter").text(score);
 }
 
 //click handler functions
-$("#crystal1").on("click", function(){
+$("#crystal1").on("click", function () {
     getClick(crystal1);
     printer();
 });
-$("#crystal2").on("click", function(){
+$("#crystal2").on("click", function () {
     getClick(crystal2);
     printer();
 });
-$("#crystal3").on("click", function(){
+$("#crystal3").on("click", function () {
     getClick(crystal3);
     printer();
 });
-$("#crystal4").on("click", function(){
+$("#crystal4").on("click", function () {
     getClick(crystal4);
     printer();
 });
